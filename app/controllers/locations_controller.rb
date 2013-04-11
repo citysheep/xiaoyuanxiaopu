9afd18ec56b2f648e4dec350d78c7d236539a259
daren.yr@gmail.com <ruiyang1214@gmail.com>
2013-04-11 22:27:37 +0800

    add missing comma

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,9 +4,9 @@ class LocationsController < ApplicationController
     def get
       respond_to do |format|
         format.json { render :json => {
-            :lat => session[:location] ? session[:location][:lat] : geo_lat
+            :lat => session[:location] ? session[:location][:lat] : geo_lat,
             :lng => session[:location] ? session[:location][:lng] : geo_lng
-			}
+			     }
         }
       end
     end
