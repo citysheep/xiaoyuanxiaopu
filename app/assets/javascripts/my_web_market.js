97b75d56328470c6d9e3fc0e3f4ec1a13e9bb620
darrenyang <daren.yr@gmail.com>
2013-11-22 00:19:45 +0800

    enhance one-click-share

--- app/assets/javascripts/my_web_market.js
+++ app/assets/javascripts/my_web_market.js
@@ -9,13 +9,6 @@ window.MyWebMarket = {
 
 $(document).ready(function(){
   MyWebMarket.init();
-  $("a.hint").tooltip('hide');
-  $("a.social-share").click(function(){
-    $("a.jiathis_button_renren").click();
-    $("a.jiathis_button_weixin").click();
-    $("a.jiathis_button_tsina").click();
-    $("a.jiathis_button_qzone").click();
-  });
 });
 
 function renderFollowButtn(){
