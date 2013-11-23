79d4b1ecdc00bc2d21e508d6a91fefce89f5ca2e
darrenyang <daren.yr@gmail.com>
2013-11-23 16:34:40 +0800

    new transparent logo message controller enhancement

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 class MessagesController < ApplicationController
     
-  before_filter :authenticate_user!, set_user
+  before_filter :authenticate_user!, :set_user
 
   def index
     if params[:mailbox] == "sent"
